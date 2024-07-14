4706c6a2da01ca0efe5ac2d0e5f592f00f5657a7
AxelSingleton <gosha@kolokutin.com>
2024-07-14 19:48:27 +0300

    Fixed [MODULE_NOT_FOUND]

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-// Importing the native module using the name specified in the type declaration
-import * as hello from 'hello';
+import * as path from 'path';
 
-// Use the function or methods exported from the module
+// Dynamically resolve the path to the hello.node module
+const helloPath = path.resolve(__dirname, '../build/Release/hello.node');
+const hello = require(helloPath);
+
 console.log(hello.hello());
